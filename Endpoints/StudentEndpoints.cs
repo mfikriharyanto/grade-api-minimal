ef30c6c9f69089686b907099b97beabc158d36e0
Muhammad Fikri Haryanto <mfharyantooo@gmail.com>
2023-08-22 21:49:46 +0700

    Update results to typedresults for better documentation

--- Endpoints/StudentEndpoints.cs
+++ Endpoints/StudentEndpoints.cs
@@ -1,6 +1,7 @@
 using Grade.Api.Dtos;
 using Grade.Api.Models;
 using Grade.Api.Repositories;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace Grade.Api.Endpoints;
 
@@ -12,42 +13,43 @@ public static class StudentsEndpoints
 
         studentRoutes.MapGet("/", async (IStudentRepository repository) =>
         {
-            return (await repository.GetAllAsync()).Select(student => student.AsDto());
+            var students = (await repository.GetAllAsync()).Select(student => student.AsDto());
+            return TypedResults.Ok(students);
         });
 
         studentRoutes.MapPost("/", async (IStudentRepository repository, CreateStudentDto studenDto) =>
         {
             Student student = new(studenDto.Name);
             await repository.CreateAsync(student);
-            return Results.CreatedAtRoute("Students", new { id = student.Id }, student.AsDto());
+            return TypedResults.CreatedAtRoute(student.AsDto(), "GetStudent", new { student.Id });
         });
 
-        studentRoutes.MapGet("/{id}", async (IStudentRepository repository, int id) =>
+        studentRoutes.MapGet("/{id}", async Task<Results<NotFound, Ok<StudentDto>>> (IStudentRepository repository, int id) =>
         {
             var student = await repository.GetAsync(id);
-            return student is null ? Results.NotFound() : Results.Ok(student.AsDto());
-        }).WithName("Students");
+            return student is null ? TypedResults.NotFound() : TypedResults.Ok(student.AsDto());
+        }).WithName("GetStudent");
 
-        studentRoutes.MapPut("/{id}", async (IStudentRepository repository, int id, UpdateStudentDto updatedStudentDto) =>
+        studentRoutes.MapPut("/{id}", async Task<Results<NotFound, NoContent>> (IStudentRepository repository, int id, UpdateStudentDto updateStudentDto) =>
         {
             var existingStudent = await repository.GetAsync(id);
 
-            if (existingStudent is null) return Results.NotFound();
+            if (existingStudent is null) return TypedResults.NotFound();
 
-            existingStudent.Name = updatedStudentDto.Name;
+            existingStudent.Name = updateStudentDto.Name;
             await repository.UpdateAsync(existingStudent);
 
-            return Results.NoContent();
+            return TypedResults.NoContent();
         });
 
-        studentRoutes.MapDelete("/{id}", async (IStudentRepository repository, int id) =>
+        studentRoutes.MapDelete("/{id}", async Task<Results<NotFound, NoContent>> (IStudentRepository repository, int id) =>
         {
             var student = await repository.GetAsync(id);
 
-            if (student is null) return Results.NotFound();
+            if (student is null) return TypedResults.NotFound();
 
             await repository.DeleteAsync(student);
-            return Results.NoContent();
+            return TypedResults.NoContent();
         });
 
         return studentRoutes;
