ceeefdba1abf6a1443fd65613cc3a2411b8f2f00
Muhammad Fikri Haryanto <mfharyantooo@gmail.com>
2023-08-22 09:28:32 +0700

    Add Swashbuckle package

--- Program.cs
+++ Program.cs
@@ -1,13 +1,32 @@
 using Grade.Api.Data;
 using Grade.Api.Entities;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRepositories(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc(
+        "v1",
+        new OpenApiInfo {
+            Title = "Grade API",
+            Version = "v1",
+        });
+});
 
 var app = builder.Build();
 
 await app.Services.InitializeDbAsync();
 
 app.MapStudentsEndpoints();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Grade API v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.Run();
